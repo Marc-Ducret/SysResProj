69f141845a78fc21c6388d4c1699a59a5699ad39
Marc <vengeurk@gmail.com>
2017-05-15 20:54:55 +0200

    tree fix

--- programs/src/tree/main.c
+++ programs/src/tree/main.c
@@ -39,26 +39,31 @@ void print_folder(char *file, int mode, int size, int offset) {
         return;
     }
     int res = readdir(fd, &dirent);
+    int err;
+
     while (res == 0) {
         dirent_t next;
+        res = readdir(fd, &next);
+        err = errno;
         
         if(dirent.name[0] != '.') {
             if (dirent.type == FILE) {
                 for(int i = 0; i < offset; i ++) printf("\xB3 ");
-                printf("%c%s", 0xC3, dirent.name);
-                if (mode || size) printf("(");
+                printf("%c%s", res ? 0xC0 : 0xC3, dirent.name);
+                if (mode || size) printf(" (");
                 if (size) print_size(dirent.size);
                 if (mode && size) printf(", ");
                 if (mode) {
                     printf("%s, %s", dirent.mode & SYSTEM ? "S":"U", 
                                       dirent.mode & RDONLY ? "RO":"RW");
                 }
+                if (mode || size) printf(")");
                 printf("\n");
             } else {
                 for(int i = 0; i < offset; i ++) printf("\xB3 ");
-                printf("\xC3\xC4\xC2%fg%s%pfg", BLUE, dirent.name);
+                printf("%c\xC4\xC2%fg%s%pfg", res ? 0xC0 : 0xC3, BLUE, dirent.name);
                 if (mode)
-                    printf("(%s, %s)", dirent.mode & SYSTEM ? "S":"U", 
+                    printf(" (%s, %s)", dirent.mode & SYSTEM ? "S":"U", 
                                       dirent.mode & RDONLY ? "RO":"RW");
                 printf("\n");
                 if(dirent.name[0] != '.') {
@@ -68,11 +73,8 @@ void print_folder(char *file, int mode, int size, int offset) {
                 }
             }
         }
-        res = readdir(fd, &next);
-        printf("cur %s, next %s\n", dirent.name, next.name);
         dirent = next;
     }
-    int err = errno;
     closedir(fd);
     if (err != ENOENT) {
         fprintf(STDERR, "\ntree: An error occured while reading %s: %s\n", file, strerror(errno));
@@ -88,7 +90,7 @@ int main(char *a) {
 
     int mode = eat_option(&args, "m");
     int size = eat_option(&args, "s");
-    remain_option(&args, "tree");
+    remain_option(&args, "t");
     
     print_folder(".", mode, size, 0);
     
